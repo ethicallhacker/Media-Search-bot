1c9cf8762f6d4c36c5ec122e5b2cce54d952351e
Bhatmanjusms <86521081+Bhatmanjusms@users.noreply.github.com>
2021-09-25 17:14:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -298,7 +298,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('Join our channel', url=f'https://t.me/filmyfunda_movies')
                     ]
                     ]
                 
@@ -329,7 +329,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{title}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('Join our channel ', url=f'https://t.me/filmyfunda_movies')
                     ]
                     ]
                 
